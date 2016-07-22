63c29d39d2ddd947a8a876d8aa8f83e044456dab
ArloParker <aaparker@mix.wvu.edu>
2016-07-22 10:57:09 +0100

    Update datagen.py

--- Example/datagen.py
+++ Example/datagen.py
@@ -133,10 +133,7 @@ def all_images():
     wtnew=Wn.real.copy()
     Wc=powerlawmod(abs(wt),abs(Wc),tab_k, int(tab_k.shape[0]*1/2) , -3.3)
     Wc=interceptmod(abs(wt), abs(Wc), tab_k, -difference)
-    #ims=[]
-    #ims2= []
-    #fig, ax = plt.subplots()
-    #fig2, ax2 = plt.subplots()
+
     
 
 #Raising Power#
@@ -145,10 +142,8 @@ def all_images():
         f_fig, f_ax = plt.subplots(2, 1, figsize=(18,18))
         f_ax[0].set_xlim(-6,0)
         f_ax[0].set_ylim(-6,14)
-        #f_fig=plt.figure()
-        #ax1=f_fig.subplot(223)
-        #ax2=f_fig.subplot(224)
-        #ax3=plt.subplot(211)
+        
+        
         wtnew=0
         wCmod=0
         wCmod=interceptmod(abs(wt), abs(Wc), tab_k, incrementsize*(i))
@@ -156,33 +151,25 @@ def all_images():
         wtnew=Wn.real+abs(wCmod)*np.cos(cphase)
         rec_image= wts.halo_inverse(wtnew, tab_k)
         y_c=np.log(np.mean((abs(wCmod)**2.), axis=(0,1)))
-        #frame=ax.imshow(rec_image.real, interpolation='none', cmap='Greys_r')
+        
         f_ax[0].plot(x, y_c)
         f_ax[0].plot(x,power)
         f_ax[1].imshow(rec_image.real, interpolation='none', cmap= 'Greys_r',)
         f_ax[0].set_xlim(-6,0)
         f_ax[0].set_ylim(-6,14) 
-        #f_ax[1].set_aspect(1)
-        #f_ax[1].set_xlim(256)
-        #f_ax[1].set_ylim(256)
-        #frame2= ax2.plot(x, y_c)
+    
         
        
         f_fig.savefig('/home/aparker/pycodes/data/modified_images/increment_'+str(i)+'.png')
-        #ims2.append([frame2])
-        #ims.append([frame])
+        
 
 
 #Changing Slopes
     for i in range(25):
         imagecount=i+24
-        #plt.close()
-        #i=i+1.
+        
         f_fig, f_ax = plt.subplots(2, 1, figsize=(18,18))
-        #f_fig=plt.figure()
-        #ax1=f_fig.subplot(223)
-        #ax2=f_fig.subplot(224)
-        #ax3=plt.subplot(211)
+        
         frac_i= (i)/25.
         wCmod=0
         wCmod=powerlawmod(abs(wt),abs(Wc),tab_k, int(tab_k.shape[0]*1/2) , -3.3+frac_i)
@@ -191,34 +178,15 @@ def all_images():
         wtnew=Wn.real+abs(wCmod)*np.cos(cphase)
         rec_image=wts.halo_inverse(wtnew,tab_k)
         y_c=np.log(np.mean((abs(wCmod)**2.), axis=(0,1)))
-        #frame=ax.imshow(rec_image.real, interpolation='none', cmap='Greys_r')
+        
     
         f_ax[0].plot(x, y_c)
         f_ax[0].plot(x,power)
         f_ax[1].imshow(rec_image.real, interpolation='none', cmap= 'Greys_r')
         f_ax[0].set_xlim(-6,0)
         f_ax[0].set_ylim(-6,14)
-        #f_ax[1].set_aspect(1)
-        #f_ax[1].set_xlim(-256,256)
-        #f_ax[1].set_ylim(-256,256)
+       
         f_fig.savefig('/home/aparker/pycodes/data/modified_images/increment_'+str(imagecount)+'.png')
-        #ims.append([frame])
-        #frame2= ax2.plot(x, y_c)
-        #ims2.append([frame2])
-    #ani2 = animation.ArtistAnimation(fig, ims, interval=50., repeat_delay=1000, blit=True)
-    #ani = animation.ArtistAnimation(fig, ims, interval=50., repeat_delay=1000, blit=True)
-    #ani.save('im.mp4',)
+       
   
 
-#histograms
-    #plt.close('all')
-    #rec_image.real=rec_image.real+1
-    #print rec_image.real.min()
-    
-    #fig, ax =plt.subplots()
-    #ax.set_xscale('log')
-    #ax.hist(rec_image.real[:150,150:].flatten(), bins=np.logspace(-1,1.,50))
-    #fig2, ax2 = plt.subplots()
-    #ax2.hist(rec_image.real[:150,:150].flatten(), bins=np.logspace(-1,1.,50))
-    #ax2.set_xscale('log')
-    #plt.show()
